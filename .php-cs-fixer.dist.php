120d5e5589b1f6222c6eabb9dc5d1fd9edfe434d
Eric Butera <eric@ericbutera.com>
2025-07-16 20:58:34 -0400

    fix: linters

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -11,6 +11,17 @@ return (new PhpCsFixer\Config())
         'no_unused_imports'      => true,
         'binary_operator_spaces' => ['default' => 'align_single_space_minimal'],
         'ordered_imports'        => ['sort_algorithm' => 'alpha'],
+        'no_extra_blank_lines'   => true,
+        'no_extra_blank_lines'   => [ // Customize blank line removal
+            'tokens' => [
+                'attribute',
+                'break',
+                'case',
+                'continue',
+                'curly_brace_block',
+            ],
+        ],
+
     ])
     ->setFinder($finder)
     ->setRiskyAllowed(true);
